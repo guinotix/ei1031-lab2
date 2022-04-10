b8416ebc8f6b60df354502ba1c87c4535614086c
Alejandro Guinot <al375927@uji.es>
2022-04-10 17:30:53 +0200

    Parameters error during execution solved

--- src/test/java/PaqueteJuego/NivelesTest.java
+++ src/test/java/PaqueteJuego/NivelesTest.java
@@ -1,8 +1,9 @@
 package PaqueteJuego;
 
-import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,39 +20,24 @@ import java.util.stream.Stream;
 @SelectPackages("PaqueteJuego")
 class NivelesTest {
 
-    private String tablaEquipos;
-    private String tablaJugadores;
-    private Niveles niveles;
-    private int esperado;
-
-    public NivelesTest(String tablaEquipos, String tablaJugadores, Niveles niveles, int esperado) {
-        this.tablaEquipos = tablaEquipos;
-        this.tablaJugadores = tablaJugadores;
-        this.niveles = niveles;
-        this.esperado = esperado;
-    }
-
-    @AfterEach
-    public void limpia() {
-        this.tablaEquipos = null;
-        this.tablaJugadores = null;
-        this.niveles = null;
-        this.esperado = -1;
-    }
-
     static Stream<Arguments> datos() {
         return Stream.of(
                 Arguments.of("jugadores.txt", "equipos.txt", new Niveles(3, 50), 0)
         );
     }
 
-    @Test
     @ParameterizedTest
     @MethodSource("datos")
     void actualizarNiveles(String jugadores, String equipos, Niveles nivel, int esperado) {
-        assertEquals(esperado, nivel.actualizarNiveles(
-                new TablaJugadores(NivelesTest.class.getResource("/" + jugadores).getPath()),
-                new TablaEquipos(NivelesTest.class.getResource("/" + equipos).getPath())
-        ));
+
+        int codigo = nivel.actualizarNiveles(
+                new TablaJugadores(TablaJugadores.class.getResource("/" + jugadores).getPath()),
+                new TablaEquipos(TablaEquipos.class.getResource("/" + equipos).getPath())
+        );
+
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Esperado: " + esperado);
+
+        assertThat(codigo, is(esperado));
     }
 }
